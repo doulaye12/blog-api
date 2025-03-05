ff5c047a8e91009b28d4913d9ef3d0561f081e39
Abdoulaye Mangane <abmangane12@gmail.com>
2025-03-05 10:43:15 +0000

    remove initial data

--- src/main/java/com/laye/blog/blogapi/BlogApiApplication.java
+++ src/main/java/com/laye/blog/blogapi/BlogApiApplication.java
@@ -14,19 +14,4 @@ public class BlogApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApiApplication.class, args);
 	}
-
-	@Bean
-	CommandLineRunner run(UserRepo userRepository) {
-		return args -> {
-			if (userRepository.findByUsername("admin").isEmpty()) {
-				User admin = new User();
-				admin.setUsername("admin");
-				admin.setPassword("admin");
-				admin.setEmail("admin@admin.com");
-				admin.setRole(Role.ADMIN);
-				userRepository.save(admin);
-				System.out.println("Admin added");
-			}
-		};
-	}
 }
